882aec3b99d98ca51d9f178c8ab751f47a351b5d
sohan d <62917789+SohanD88@users.noreply.github.com>
2026-06-20 00:44:40 -0400

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        balanced = True
+
+        def dfs(root):
+            nonlocal balanced
+
+            if not root:
+                return 0
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if abs(left - right) > 1:
+                balanced = False
+
+            return 1 + max(left, right)
+
+        dfs(root)
+        return balanced
