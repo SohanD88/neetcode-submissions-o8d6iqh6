31ac6c809edf0f26ca2c88460827d9c683e7d319
sohan d <62917789+SohanD88@users.noreply.github.com>
2026-06-27 10:17:18 +0200

    Add: add-two-numbers - submission-4

---
+++ Data Structures & Algorithms/add-two-numbers/submission-4.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy = ListNode(0, None)
+        dumCur = dummy
+        curr1 = l1
+        curr2 = l2
+
+        carryOver = 0
+
+        while curr1 or curr2:
+            if not curr1:
+                cursum = curr2.val + carryOver
+                if cursum >= 10:
+                    cursum = cursum - 10
+                    carryOver = 1
+                else:
+                    carryOver = 0
+                dumCur.next = ListNode(cursum, None)
+                dumCur = dumCur.next
+                curr2 = curr2.next
+
+            elif not curr2:
+                cursum = curr1.val + carryOver
+                if cursum >= 10:
+                    cursum = cursum - 10
+                    carryOver = 1
+                else:
+                    carryOver = 0
+                dumCur.next = ListNode(cursum, None)
+                dumCur = dumCur.next
+                curr1 = curr1.next
+
+            else:
+                cursum = curr1.val + curr2.val + carryOver
+                if cursum >= 10:
+                    cursum = cursum - 10
+                    carryOver = 1
+                else:
+                    carryOver = 0
+                dumCur.next = ListNode(cursum, None)
+                dumCur = dumCur.next
+
+                curr1 = curr1.next
+                curr2 = curr2.next
+
+        if carryOver > 0:
+            dumCur.next = ListNode(carryOver, None)
+
+        dummy = dummy.next
+        return dummy
+
